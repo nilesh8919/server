3ad4414351f6a3cf957d9f626c08000dc9fa73bc
nsonekar <nsonekar@valethi.in>
2025-06-12 12:33:53 +0530

    add new

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Node + MongoDB app is running!');
+  res.send('Node + MongoDB app is running2!');
 });
 app.get('/getMsg', (req, res) => {
     res.send('Node + MongoDB app is running2!');
