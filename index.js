52cd5cd1635c154e8bd7274f9ccd997dfd0d5142
nsonekar <nsonekar@valethi.in>
2025-06-17 10:43:24 +0530

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Node + MongoDB app is running9!');
+  res.send('Node + MongoDB app is running10!');
 });
 app.get('/getMsg', (req, res) => {
     res.send('Node + MongoDB app is running9!');
