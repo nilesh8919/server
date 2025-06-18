96f860d51277074b603fa2ab254c2ecc676d077b
nsonekar <nsonekar@valethi.in>
2025-06-18 16:59:33 +0530

    first commit

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ app.get('/getMsg', (req, res) => {
   });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
