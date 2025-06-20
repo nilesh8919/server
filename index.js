e2dcafb4a78fe2946a744a712e2aee28cf0c077f
nsonekar <nsonekar@valethi.in>
2025-06-20 13:52:43 +0530

    add new

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-/*mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err)); */
+.catch(err => console.error('MongoDB connection error:', err)); 
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Node + MongoDB app is running7!');
+  res.send('Node + MongoDB app is running8!');
 });
 
 
